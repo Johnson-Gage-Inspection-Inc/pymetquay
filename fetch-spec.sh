5eb0979a0c98981a5132f7732a32cfb88fedb7eb
Jeff Hall <rhythmatician5@gmail.com>
2026-02-19 17:35:04 -0600

    Add JSON formatting step to fetch-spec.sh for improved readability

--- fetch-spec.sh
+++ fetch-spec.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 curl "https://app.swaggerhub.com/apiproxy/registry/metquayinc-c94/Metquay.API.CRUD/1.0.0-jgi?format=json" > 1.0.json
+python -c "import json; data = json.load(open('1.0.json')); json.dump(data, open('1.0.json', 'w'), ensure_ascii=False, indent=4)"
